29a243a80bd4165e44d13da490300e571a03c9a7
Giel Dops <gieldops@hotmail.com>
2020-12-06 10:33:16 +0100

    Day 6 are both oneliners now

--- day06/solution2.py
+++ day06/solution2.py
@@ -1,10 +1,4 @@
 with open("input.txt", "r") as f:
     groups = [g.strip() for g in f.read().split("\n\n")]
 
-num = 0
-for g in groups:
-    lines = g.split("\n")
-    for c in set(g.replace("\n", "")):
-        if all(c in l for l in lines):
-            num += 1
-print(num)
+print(sum(1 for g in groups for c in set(g.replace("\n", "")) if all(c in l for l in g.split("\n"))))
