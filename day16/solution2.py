c8eeb6df876e880cd5f09f6ccb2b7f59cfe56339
Giel Dops <gieldops@hotmail.com>
2020-12-20 21:28:11 +0100

    Improved part 2 of day 16

--- day16/solution2.py
+++ day16/solution2.py
@@ -12,29 +12,22 @@ for field_raw in fields_raw:
 your_ticket = [int(i) for i in your_ticket_raw[1].split(",")]
 nearby_tickets = [[int(i) for i in l.split(",")] for l in nearby_tickets_raw[1:]]
 
-valid_tickets = []
+field_assignment = {field_i: set(fields.keys()) for field_i in range(len(your_ticket))}
 for ticket in nearby_tickets:
-    for ticket_field in ticket:
+    for i, ticket_field in enumerate(ticket):
         if not any(ticket_field in field_range for field_range in fields.values()):
             break
-    else:
-        valid_tickets.append(ticket)
+        to_remove = {name for name in field_assignment[i] if ticket_field not in fields[name]}
+        field_assignment[i] -= to_remove
 
-field_placement = {field_i: set(fields.keys()) for field_i in range(len(your_ticket))}
-for ticket in valid_tickets:
-    for i, ticket_field in enumerate(ticket):
-        currently_possible_fields = field_placement[i]
-        to_remove = {name for name in currently_possible_fields if ticket_field not in fields[name]}
-        field_placement[i] -= to_remove
-
-final_field_placement: Dict[int, str] = {}
-for i, field_names in sorted(field_placement.items(), key=lambda t: len(t[1])):
-    field_names -= set(final_field_placement.values())
+final_field_assignment: Dict[int, str] = {}
+for i, field_names in sorted(field_assignment.items(), key=lambda t: len(t[1])):
+    field_names -= set(final_field_assignment.values())
     if len(field_names) == 1:
-        final_field_placement[i] = next(iter(field_names))
+        final_field_assignment[i] = next(iter(field_names))
 
 solution = 1
-for i, field_name in final_field_placement.items():
+for i, field_name in final_field_assignment.items():
     if field_name.startswith("departure"):
         solution *= your_ticket[i]
 print(solution)
